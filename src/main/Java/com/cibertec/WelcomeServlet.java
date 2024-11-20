9bded241f47e6e7dff3903a53d88143c095b7aab
diegosg05 <rextoon012@gmail.com>
2024-11-19 23:11:33 -0500

    Se crearon nuevos Servlet para el proyecto

---
+++ src/main/Java/com/cibertec/WelcomeServlet.java
@@ -0,0 +1,20 @@
+package com.cibertec;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/welcome") //Generar codigo necesario para exponer el servlet
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hola desde el Servlet Welcome</h1>");
+    }
+}
